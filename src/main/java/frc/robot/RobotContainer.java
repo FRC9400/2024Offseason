dbd9dfad629284cba64e9160508be81e298eb667
josie-miller <josephinemiller13524@icloud.com>
2024-03-01 18:30:29 -0800

    swerve teleop

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,18 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.SwerveControlRequestParameters;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.proto.Wpimath;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Subsystems.Elevator.Elevator;
 import frc.robot.Subsystems.Elevator.ElevatorIOTalonFX;
@@ -17,6 +24,7 @@ import frc.robot.Subsystems.Intake.IntakeIO;
 import frc.robot.Subsystems.Intake.IntakeIOTalonFX;
 import frc.robot.Subsystems.Shooter.Shooter;
 import frc.robot.Subsystems.Shooter.ShooterIOTalonFX;
+import frc.robot.Subsystems.Swerve.Swerve;
 import frc.robot.Constants.canIDConstants;
 
 public class RobotContainer {
@@ -25,9 +33,11 @@ public class RobotContainer {
   private final Intake s_intake = new Intake(new IntakeIOTalonFX(canIDConstants.intakeMotor));
   private final Elevator s_elevator = new Elevator(new ElevatorIOTalonFX(canIDConstants.leftElevatorMotor, canIDConstants.rightElevatorMotor));
   private final Shooter s_shooter = new Shooter(new ShooterIOTalonFX(canIDConstants.handoverMotor, canIDConstants.leftShooterMotor, canIDConstants.rightShooterMotor));
-
+  private final Swerve s_swerve = new Swerve();
   public RobotContainer() {
     configureBindings();
+    configureDefaultCommands();
+
   }
 
   private void configureBindings() {
@@ -36,8 +46,24 @@ public class RobotContainer {
     .onFalse(new InstantCommand(() -> s_intake.requestIdle()));
 
 
+
+    
+
     }
-  
+  private void configureDefaultCommands() {
+
+    s_swerve.setDefaultCommand(
+      new RunCommand(() -> {
+        double xSpeed = MathUtil.applyDeadband(-controller.getLeftY(), .05); 
+        double ySpeed = MathUtil.applyDeadband(controller.getLeftX(), .05);
+        double rot = MathUtil.applyDeadband(controller.getRightX(), 0.05);
+        xSpeed = xSpeed * Math.abs(xSpeed);
+        ySpeed = ySpeed * Math.abs(ySpeed);
+        rot = rot * Math.abs(rot);
+        s_swerve.requestPercent(xSpeed, ySpeed, rot, true); 
+      }, s_swerve)
+    );
+  } 
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
