ea451aa88c5311b1a7a7dc645db1d720524bb7b4
Aiden Shi <shiaiden@gmail.com>
2024-10-06 13:41:02 -0700

    hi

--- src/main/java/frc/robot/autons/modes/TEST.java
+++ src/main/java/frc/robot/autons/modes/TEST.java
@@ -1,11 +1,19 @@
 package frc.robot.autons.modes;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Subsystems.Superstructure;
+import com.choreo.lib.Choreo;
 import frc.robot.Subsystems.Swerve.Swerve;
 
 public class TEST extends SequentialCommandGroup {
 
-    public TEST(Swerve swerve, Superstructure superstructure){}
+    public TEST(Swerve swerve, Superstructure superstructure){
+        addRequirements(swerve, superstructure);
+        addCommands(
+            new InstantCommand(() -> swerve.setGyroStartingPosition(0)),
+            swerve.runChoreoTraj(Choreo.getTrajectory("test"),true)
+        );
+    }
     
 }
