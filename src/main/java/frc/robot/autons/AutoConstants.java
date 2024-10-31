98e5a4f6665e805041dca66453a32d648dcc4d6a
josie-miller <josephinemiller13524@icloud.com>
2024-10-30 17:27:57 -0700

    hi

--- src/main/java/frc/robot/autons/AutoConstants.java
+++ src/main/java/frc/robot/autons/AutoConstants.java
@@ -8,28 +8,28 @@ public class AutoConstants {
     public static final PIDController choreoRotController = new PIDController(1.25,0,0);
 
     public static double subwooferVelM = 70;
-    public static double subwooferRatioM = 0.5;
-    public static double subwooferDegM = 45;
+    public static double subwooferRatioM = 0.6;
+    public static double subwooferDegM = 45; //done
 
-    public static double subwooferVelR = 70;
-    public static double subwooferRatioR = 0.5;
-    public static double subwooferDegR = 45;
+    public static double subwooferVelR = 60;
+    public static double subwooferRatioR = 0.7;
+    public static double subwooferDegR = 45; //done
 
     public static double subwooferVelL = 45.6;
     public static double subwooferRatioL = 1.5;
-    public static double subwooferDegL = 50;
+    public static double subwooferDegL = 45;
 
     public static double VelM = 70;
     public static double RatioM = 0.6;
-    public static double DegM = 30;
+    public static double DegM = 25;
 
     public static double VelR = 70;
     public static double RatioR = 0.5;
-    public static double DegR = 30;
+    public static double DegR = 25;
 
-    public static double VelL = 45.6;
-    public static double RatioL = 1.5;
-    public static double DegL = 30;
+    public static double VelL = 70;
+    public static double RatioL = 0.5;
+    public static double DegL = 25;
 
     
 }
